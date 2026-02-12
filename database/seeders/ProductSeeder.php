3a6c743e8e1dd7f375c62529849cef3748b9037f
bruno-keil <bruno.keil@estudante.ufjf.br>
2026-02-12 13:06:02 -0300

    fix(seeder): update ProductSeeder data

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -17,9 +17,20 @@ class ProductSeeder extends Seeder
         if ($categories->isEmpty()) {
             $categories = Category::factory()->count(6)->create();
         }
+        
+        $testUser = User::where('email', 'teste@email.com')->first();
+
+        if ($testUser) {
+            Product::factory()
+                ->count(5)
+                ->create([
+                    'user_id' => $testUser->id,
+                    'categoria_id' => $categories->random()->id,
+                ]);
+        }
 
         Product::factory()
-            ->count(36)
+            ->count(31)
             ->sequence(function ($sequence) use ($users, $categories) {
                 return [
                     'user_id' => $users->random()->id,

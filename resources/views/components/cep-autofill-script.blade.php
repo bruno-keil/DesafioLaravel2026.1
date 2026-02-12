a1bc5b6b60e7e06a074c9a042646f3e9bd6b603e
bruno-keil <bruno.keil@estudante.ufjf.br>
2026-02-12 13:56:01 -0300

    feat: update CEP autofill script to use new API endpoint

--- resources/views/components/cep-autofill-script.blade.php
+++ resources/views/components/cep-autofill-script.blade.php
@@ -2,7 +2,7 @@
     document.getElementById('cep').addEventListener('input', function(e) {
         let cep = e.target.value.replace(/\D/g, '');
         if (cep.length === 8) {
-            fetch(`https://viacep.com.br/ws/${cep}/json/`)
+            fetch(`/api/cep/${cep}`)
                 .then(response => response.json())
                 .then(data => {
                     if (!data.erro) {
